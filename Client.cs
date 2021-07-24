cdbc0878f451e6de066bf7f676090c19e2bb0a3e
Marc Martinez <universal11@gmail.com>
2021-07-23 21:37:10 -0700

    changed methods to return HttpResponseMessage

--- Client.cs
+++ Client.cs
@@ -10,10 +10,12 @@ namespace PUBGAPI
     public class Client
     {
         public string apiKey{get;set;}
+        /*
         public bool enableDebugMode {get;set;} = false;
         private int requestDelayInSeconds {get;set;} = 1;
         private int requestLimit {get;set;} = 30;
         private int requestCounter {get;set;} = 0;
+        */
         private HttpClient httpClient = new HttpClient();
 
     
@@ -25,28 +27,8 @@ namespace PUBGAPI
             this.apiKey = apiKey;
         }
 
-        /*
-        public void Authenticate(){
-            this.accessToken = this.GetAccessToken();
-        }
-        public string GetAccessToken(){
-            string url = $"oauth/token";
-            Dictionary<string, string> values = new Dictionary<string, string>{
-                { "grant_type", "client_credentials" }
-            };
-            FormUrlEncodedContent data = new FormUrlEncodedContent(values);
-            this.httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes($"{this.clientId}:{this.secretKey}")) );
-            if(this.enableDebugMode){
-                System.Console.WriteLine($"POST: {url}");
-            }
-            HttpResponseMessage response = this.httpClient.PostAsync($"{url}", data).Result;
-            string accessToken = JObject.Parse(response.Content.ReadAsStringAsync().Result)["access_token"].ToString();
-            return accessToken;
-
-        }
-        */
-
         public HttpResponseMessage HttpGetWithAuth(string url){
+            /*
             if(this.requestCounter == this.requestLimit){
                 System.Threading.Thread.Sleep(this.requestDelayInSeconds * 1000);
                 this.requestCounter = 0;
@@ -55,15 +37,19 @@ namespace PUBGAPI
             if(this.enableDebugMode){
                 System.Console.WriteLine($"GET: {url}");
             }
+            */
 
             this.httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this.apiKey);
             this.httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.api+json"));
             HttpResponseMessage response = this.httpClient.GetAsync(url).Result;
+            /*
             if(!response.IsSuccessStatusCode){
                 System.Console.WriteLine($"Error Code: {response.StatusCode} | {response.Content.ReadAsStringAsync().Result}");
                 return null;
             }
+            
             this.requestCounter++;
+            */
             return response;
         }
 
@@ -71,13 +57,10 @@ namespace PUBGAPI
 
         }
 
-        public void getPlayers(List<string> accountIds, List<string> playerNames){
+        public HttpResponseMessage getPlayers(List<string> accountIds, List<string> playerNames){
             string url = $"https://api.pubg.com/shards/steam/players?filter[playerIds]={String.Join(",", accountIds)}&filter[playerNames]={String.Join(",", playerNames)}";
-            System.Console.WriteLine("getPlayers");
-            System.Console.WriteLine(url);
-
-            HttpResponseMessage response = this.HttpGetWithAuth(url);
-            System.Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+            return this.HttpGetWithAuth(url);
+            //System.Console.WriteLine(response.Content.ReadAsStringAsync().Result);
         }
         
         
